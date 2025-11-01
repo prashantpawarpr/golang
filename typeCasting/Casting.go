f3ff0a0879c0529a613904548080c8650b64b2fb
prashantpawarpr <prashantpawarpr@gmail.com>
2025-11-01 18:40:26 +0530

    check the real and imaginary number in the complex number and how to access it

--- typeCasting/Casting.go
+++ typeCasting/Casting.go
@@ -6,9 +6,18 @@ func main() {
 	var a int32 = 56
 	var b float32 = 1321.13
 
-	fmt.Println("hello brother")
+	var str rune = 97
+
+	fmt.Printf("%s \n", string(str))
+
 	fmt.Printf("%d", a)
 	fmt.Printf("%f", b)
 	c := int32(a) + int32(b)
 	fmt.Println("the value of c is:", c)
+
+	comp := 3 + 22i
+
+	reaNum := real(comp)
+	imaNum := imag(comp)
+	fmt.Println(reaNum, imaNum)
 }
